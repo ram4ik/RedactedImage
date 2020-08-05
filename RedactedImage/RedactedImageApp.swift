b82bf06947452d3b76f3fb2950257b40aaae70eb
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-05 11:27:17 +0300

    Initial Commit

---
+++ RedactedImage/RedactedImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedactedImageApp.swift
+//  RedactedImage
+//
+//  Created by ramil on 05.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct RedactedImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
